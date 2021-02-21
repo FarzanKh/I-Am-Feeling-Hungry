75f7d19514b8f5c5ca7634cee35872c617873381
Mengshi24 <58278376+Mengshi24@users.noreply.github.com>
2021-02-20 20:33:01 -0800

    Revert "Some progress for API connection"

--- app/src/main/java/com/team6/iamfeelinghungry/MainActivity.java
+++ app/src/main/java/com/team6/iamfeelinghungry/MainActivity.java
@@ -2,9 +2,7 @@ package com.team6.iamfeelinghungry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onClickSubmit(View view) {
-        Intent intent = new Intent(this, RestaurantList.class);
-        startActivity(intent);
     }
 }
