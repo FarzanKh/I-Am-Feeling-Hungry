f210896bc1a0749e1be131d82d3e1d75afaae266
Farzan <farzan_khoobchehr@yahoo.com>
2021-02-20 19:51:25 -0800

    API Data retrieval (In progress)

---
+++ app/src/main/java/com/team6/iamfeelinghungry/YelpService.kt
@@ -0,0 +1,14 @@
+package com.team6.iamfeelinghungry
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface YelpService {
+    @GET("businesses/search")
+    fun searchRestaurant(
+            @Header("Authorization") authHeader: String,
+            @Query("term") searchTerm: String,
+            @Query("location") location: String) : Call<Any>
+}
