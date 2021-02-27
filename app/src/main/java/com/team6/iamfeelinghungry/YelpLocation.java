ab70a8cd48b365845a06980ce32ffd33f83fc2e4
Farzan <farzan_khoobchehr@yahoo.com>
2021-02-26 19:22:41 -0800

    few changes

---
+++ app/src/main/java/com/team6/iamfeelinghungry/YelpLocation.java
@@ -0,0 +1,37 @@
+package com.team6.iamfeelinghungry;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class YelpLocation {
+    @SerializedName("address1")
+    @Expose
+    private String address1;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public YelpLocation() {
+    }
+
+    /**
+     *
+     * @param address1
+     */
+    public YelpLocation(String address1) {
+        super();
+        this.address1 = address1;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+}
