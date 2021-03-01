699410d73c480c2156b9f6bf8e1ceb7e817baa4f
Mengshi24 <l806685641@gmail.com>
2021-02-28 19:11:06 -0800

    Add some UI changes.

--- app/src/main/java/com/team6/iamfeelinghungry/QuizActivity.java
+++ app/src/main/java/com/team6/iamfeelinghungry/QuizActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -19,6 +21,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // remove bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_quiz);
     }
 
